03d3a46d1545ef84785cdb19c0d4de35a43c7ddb
Eason0210 <aqua0210@163.com>
2021-11-11 00:12:44 +0800

    Dont't signing

--- profiles/home-manager/personal.nix
+++ profiles/home-manager/personal.nix
@@ -2,9 +2,9 @@
   programs.git = {
     userEmail = "aqua0210@163.com";
     userName = "Eason Huang";
-    signing = {
-      key = "aqua0210@163.com";
-      signByDefault = true;
-    };
+    # signing = {
+    #   key = "aqua0210@163.com";
+    #   signByDefault = true;
+    # };
   };
 }
