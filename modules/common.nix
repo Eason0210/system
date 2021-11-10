f8024ad2c1f651837ae7c0bc7b50640fdb457b91
Eason0210 <aqua0210@163.com>
2021-11-10 19:21:17 +0800

    Update user description

--- modules/common.nix
+++ modules/common.nix
@@ -8,7 +8,7 @@
   };
 
   user = {
-    description = "Kennan LeJeune";
+    description = "Eason Huang";
     home = "${
         if pkgs.stdenvNoCC.isDarwin then "/Users" else "/home"
       }/${config.user.name}";
