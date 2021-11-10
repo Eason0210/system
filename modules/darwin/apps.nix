18cc8af32cf320c38baa756fb1df95516e6f6e79
Eason Huang <aqua0210@163.com>
2021-11-10 13:22:53 +0800

    Update homebrew apps

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -1,27 +1,16 @@
 { config, lib, pkgs, ... }: {
   homebrew = {
     casks = [
-      "1password-beta"
       "appcleaner"
-      "bartender"
       "displaperture"
-      "firefox-beta"
-      "fork"
-      "gpg-suite"
-      "gswitch"
+      "hammerspoon"
       "iina"
-      "intellij-idea"
       "karabiner-elements"
-      "keepingyouawake"
-      "keybase"
-      "kitty"
       "maccy"
       "raycast"
       "skim"
       "stats"
-      "syncthing"
-      "visual-studio-code"
-      "zoom"
+      # "syncthing"
     ];
     masApps = { };
   };
