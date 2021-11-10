9207ebb2202b530cb732ff62bd705e829f1be907
Eason0210 <aqua0210@163.com>
2021-11-10 19:00:54 +0800

    Update darwin preferences

--- modules/darwin/preferences.nix
+++ modules/darwin/preferences.nix
@@ -1,73 +1,46 @@
 { config, pkgs, ... }: {
   system.defaults = {
-    # login window settings
-    loginwindow = {
-      # disable guest account
-      GuestEnabled = false;
-      # show name instead of username
-      SHOWFULLNAME = false;
+    NSGlobalDomain = {
+      # AppleKeyboardUIMode = 3;
+      # ApplePressAndHoldEnabled = false;
+      # InitialKeyRepeat = 10;
+      # KeyRepeat = 1;
+      # _HIHideMenuBar = true;
+
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+
+      "com.apple.keyboard.fnState" = true;
+      "com.apple.mouse.tapBehavior" = 1;
+      "com.apple.sound.beep.volume" = "0.0";
+      "com.apple.sound.beep.feedback" = 0;
     };
 
-    # file viewer settings
+    ".GlobalPreferences" = {
+      "com.apple.sound.beep.sound" = "/System/Library/Sounds/Funk.aiff";
+    };
+
+    dock = {
+      autohide = true;
+      showhidden = true;
+      launchanim = false;
+      # orientation = "right";
+    };
+
     finder = {
       AppleShowAllExtensions = true;
-      FXEnableExtensionChangeWarning = true;
-      _FXShowPosixPathInTitle = true;
+      QuitMenuItem = true;
+      FXEnableExtensionChangeWarning = false;
     };
 
-    # trackpad settings
     trackpad = {
-      # silent clicking = 0, default = 1
-      ActuationStrength = 0;
-      # enable tap to click
       Clicking = true;
-      # firmness level, 0 = lightest, 2 = heaviest
-      FirstClickThreshold = 1;
-      # firmness level for force touch
-      SecondClickThreshold = 1;
-      # don't allow positional right click
-      TrackpadRightClick = false;
-      # three finger drag for space switching
-      # TrackpadThreeFingerDrag = true;
-    };
-
-    # firewall settings
-    alf = {
-      # 0 = disabled 1 = enabled 2 = blocks all connections except for essential services
-      globalstate = 1;
-      loggingenabled = 0;
-      stealthenabled = 1;
-    };
-
-    # dock settings
-    dock = {
-      # auto show and hide dock
-      autohide = true;
-      # remove delay for showing dock
-      autohide-delay = "0.0";
-      # how fast is the dock showing animation
-      autohide-time-modifier = "1.0";
-      tilesize = 50;
-      static-only = false;
-      showhidden = false;
-      show-recents = false;
-      show-process-indicators = true;
-      orientation = "bottom";
-      mru-spaces = false;
-    };
-
-    NSGlobalDomain = {
-      "com.apple.sound.beep.feedback" = 0;
-      "com.apple.sound.beep.volume" = "0.000";
-      # allow key repeat
-      ApplePressAndHoldEnabled = false;
-      # delay before repeating keystrokes
-      InitialKeyRepeat = 10;
-      # delay between repeated keystrokes upon holding a key
-      KeyRepeat = 1;
-      AppleShowAllExtensions = true;
-      AppleShowScrollBars = "Automatic";
+      TrackpadThreeFingerDrag = true;
     };
   };
-
 }
