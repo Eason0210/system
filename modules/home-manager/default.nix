3537fec860c240d5de30b6b7b32b90ed8c4173be
Eason0210 <aqua0210@163.com>
2021-11-11 22:19:38 +0800

    Add packages in home.packages

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -11,7 +11,8 @@ let
     (${sysDoNixos}) || (${sysDoDarwin})
   '');
 
-in {
+in
+{
   imports = [ ./vim ./cli ./kitty ./dotfiles ./git.nix ];
 
   programs.home-manager = {
@@ -19,74 +20,100 @@ in {
     path = "${config.home.homeDirectory}/.nixpkgs/modules/home-manager";
   };
 
-  home = let NODE_GLOBAL = "${config.home.homeDirectory}/.node-packages";
-  in {
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "20.09";
-    sessionVariables = {
-      GPG_TTY = "/dev/ttys000";
-      EDITOR = "nvim";
-      VISUAL = "nvim";
-      CLICOLOR = 1;
-      LSCOLORS = "ExFxBxDxCxegedabagacad";
-      JAVA_HOME = "${pkgs.openjdk.home}";
-      NODE_PATH = "${NODE_GLOBAL}/lib";
-      HOMEBREW_NO_AUTO_UPDATE = 1;
-    };
-    sessionPath = [ "${NODE_GLOBAL}/bin" ];
+  home =
+    let NODE_GLOBAL = "${config.home.homeDirectory}/.node-packages";
+    in
+    {
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      stateVersion = "20.09";
+      sessionVariables = {
+        GPG_TTY = "/dev/ttys000";
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+        CLICOLOR = 1;
+        LSCOLORS = "ExFxBxDxCxegedabagacad";
+        JAVA_HOME = "${pkgs.openjdk.home}";
+        NODE_PATH = "${NODE_GLOBAL}/lib";
+        HOMEBREW_NO_AUTO_UPDATE = 1;
+      };
+      sessionPath = [ "${NODE_GLOBAL}/bin" ];
 
-    # define package definitions for current user environment
-    packages = with pkgs; [
-      # python with default packages
-      (python3.withPackages
-        (ps: with ps; [ black numpy scipy networkx matplotlib ]))
-      # awscli2
-      cachix
-      # comma
-      coreutils-full
-      curl
-      emacsGit
-      fd
-      gawk
-      ghc
-      git
-      gnugrep
-      gnupg
-      gnused
-      # google-cloud-sdk
-      htop
-      httpie
-      jq
-      neofetch
-      nixUnstable
-      nixfmt
-      nixpkgs-fmt
-      # nix-scripts
-      nodejs
-      openjdk
-      openssh
-      pandoc
-      pre-commit
-      poetry
-      ranger
-      (ruby.withPackages (ps: with ps; [ rufo solargraph ]))
-      ripgrep
-      ripgrep-all
-      rsync
-      sysdo
-      tealdeer
-      # tectonic
-      treefmt
-      vagrant
-      yarn
-      youtube-dl
-    ];
-  };
+      # define package definitions for current user environment
+      packages = with pkgs; let exe = haskell.lib.justStaticExecutables; in
+      [
+        # Haskell tools
+        (exe haskellPackages.cabal-install)
+        (exe haskellPackages.hpack)
+        (exe haskellPackages.haskell-language-server)
+        (exe haskellPackages.hlint)
+        (exe haskellPackages.hindent)
+        (exe haskellPackages.ormolu)
+        (exe haskellPackages.hie-bios)
+        (exe haskellPackages.implicit-hie)
+        # C/C++ tools
+        clang-tools
+        cppcheck
+        # python with default packages
+        (python3.withPackages
+          (ps: with ps; [ black numpy scipy networkx matplotlib ]))
+        aspell
+        aspellDicts.en
+        # awscli2
+        cachix
+        # comma
+        coreutils-full
+        coq
+        curl
+        emacsGit
+        fd
+        gawk
+        ghc
+        git
+        gnugrep
+        gnupg
+        gnused
+        # google-cloud-sdk
+        hugo
+        imagemagick
+        htop
+        httpie
+        jq
+        mpv
+        neofetch
+        nixUnstable
+        nixfmt
+        nixpkgs-fmt
+        nodejs
+        nodePackages.eslint
+        openjdk
+        openssh
+        pandoc
+        pre-commit
+        poetry
+        pstree
+        ranger
+        (ruby.withPackages (ps: with ps; [ rufo solargraph ]))
+        ripgrep
+        ripgrep-all
+        rsync
+        rust-analyzer
+        rustup
+        shfmt
+        sqlite
+        sysdo
+        tealdeer
+        # tectonic
+        treefmt
+        vagrant
+        yarn
+        youtube-dl
+      ];
+    };
 }
