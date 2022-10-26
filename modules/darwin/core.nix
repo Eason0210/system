fb493293655569043e22bdccb67faf8e50ec9183
Eason0210 <aqua0210@163.com>
2022-10-26 22:46:39 +0800

    Update modules/darwin/core.nix

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -1,10 +1,12 @@
 { inputs, config, pkgs, ... }:
-let prefix = "/run/current-system/sw/bin";
-in {
+let
+  prefix = "/run/current-system/sw/bin";
+  inherit (pkgs.stdenvNoCC) isAarch64 isAarch32;
+in
+{
   # environment setup
   environment = {
     loginShell = pkgs.zsh;
-    pathsToLink = [ "/Applications" ];
     etc = { darwin.source = "${inputs.darwin}"; };
     # Use a custom configuration.nix location.
     # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
@@ -13,14 +15,16 @@ in {
     # systemPackages = [ ];
   };
 
-  fonts.enableFontDir = true;
-  nix.nixPath = [ "darwin=/etc/${config.environment.etc.darwin.target}" ];
-  nix.extraOptions = ''
-    extra-platforms = x86_64-darwin aarch64-darwin
-  '';
+  homebrew.brewPrefix = if isAarch64 || isAarch32 then "/opt/homebrew/bin" else "/usr/local/bin";
 
   # auto manage nixbld users with nix darwin
-  users.nix.configureBuildUsers = true;
+  nix = {
+    configureBuildUsers = true;
+    nixPath = [ "darwin=/etc/${config.environment.etc.darwin.target}" ];
+    extraOptions = ''
+      extra-platforms = x86_64-darwin aarch64-darwin
+    '';
+  };
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
