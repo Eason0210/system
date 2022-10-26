f3ff1d47e1d427a6f83e8f915d60b08096e98bef
Eason0210 <aqua0210@163.com>
2022-10-26 22:08:45 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,114 +2,133 @@
   description = "nix system configurations";
 
   nixConfig = {
-    substituters =
-      [ "https://eason0210.cachix.org" "https://nix-community.cachix.org/" ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+      "https://eason0210.cachix.org"
+    ];
 
     trusted-public-keys = [
-      "eason0210.cachix.org-1:+3t6gpdGWBJcwVoV7gNMSzKCxxlxqS+YrY6YCynvgSg="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "eason0210.cachix.org-1:+3t6gpdGWBJcwVoV7gNMSzKCxxlxqS+YrY6YCynvgSg="
     ];
   };
 
   inputs = {
-    darwin-stable.url = "github:nixos/nixpkgs/nixpkgs-21.05-darwin";
-    devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-    nixos-stable.url = "github:nixos/nixpkgs/nixos-21.05";
+    # package repos
+    stable.url = "github:nixos/nixpkgs/nixos-22.05";
     nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    comma = {
-      url = "github:nix-community/comma";
-      flake = false;
-    };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    small.url = "github:nixos/nixpkgs/nixos-unstable-small";
+
+    # system management
+    nixos-hardware.url = "github:nixos/nixos-hardware";
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # shell stuff
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Emacs
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
-  outputs = inputs@{ self, nixpkgs, darwin, home-manager, nixos-hardware
-    , devshell, flake-utils, ... }:
+  outputs = inputs@{ self, darwin, home-manager, flake-utils, ... }:
     let
-      inherit (darwin.lib) darwinSystem;
-      inherit (nixpkgs.lib) nixosSystem;
-      inherit (home-manager.lib) homeManagerConfiguration;
-      inherit (flake-utils.lib) eachDefaultSystem eachSystem;
-      inherit (builtins) listToAttrs map;
+      inherit (flake-utils.lib) eachSystemMap;
 
-      mkLib = nixpkgs:
-        nixpkgs.lib.extend
-        (final: prev: (import ./lib final) // home-manager.lib);
-
-      lib = (mkLib nixpkgs);
-
-      isDarwin = system: (builtins.elem system lib.platforms.darwin);
+      isDarwin = system:
+        (builtins.elem system inputs.nixpkgs.lib.platforms.darwin);
       homePrefix = system: if isDarwin system then "/Users" else "/home";
+      defaultSystems =
+        [ "x86_64-darwin" "x86_64-linux" ];
 
       # generate a base darwin configuration with the
       # specified hostname, overlays, and any extraModules applied
-      mkDarwinConfig = { system ? "x86_64-darwin", nixpkgs ? inputs.nixpkgs
-        , stable ? inputs.darwin-stable, lib ? (mkLib nixpkgs), baseModules ? [
-          home-manager.darwinModules.home-manager
-          ./modules/darwin
-        ], extraModules ? [ ] }:
-        darwinSystem {
+      mkDarwinConfig =
+        { system ? "x86_64-darwin"
+        , nixpkgs ? inputs.nixpkgs
+        , stable ? inputs.stable
+        , baseModules ? [
+            home-manager.darwinModules.home-manager
+            ./modules/darwin
+          ]
+        , extraModules ? [ ]
+        }:
+        inputs.darwin.lib.darwinSystem {
           inherit system;
           modules = baseModules ++ extraModules;
-          specialArgs = { inherit inputs lib nixpkgs stable; };
+          specialArgs = { inherit self inputs nixpkgs; };
         };
 
       # generate a base nixos configuration with the
       # specified overlays, hardware modules, and any extraModules applied
-      mkNixosConfig = { system ? "x86_64-linux", nixpkgs ? inputs.nixos-unstable
-        , stable ? inputs.nixos-stable, lib ? (mkLib nixpkgs), hardwareModules
+      mkNixosConfig =
+        { system ? "x86_64-linux"
+        , nixpkgs ? inputs.nixos-unstable
+        , stable ? inputs.stable
+        , hardwareModules
         , baseModules ? [
-          home-manager.nixosModules.home-manager
-          ./modules/nixos
-        ], extraModules ? [ ] }:
-        nixosSystem {
+            home-manager.nixosModules.home-manager
+            ./modules/nixos
+          ]
+        , extraModules ? [ ]
+        }:
+        nixpkgs.lib.nixosSystem {
           inherit system;
           modules = baseModules ++ hardwareModules ++ extraModules;
-          specialArgs = { inherit inputs lib nixpkgs stable; };
+          specialArgs = { inherit self inputs nixpkgs; };
         };
 
       # generate a home-manager configuration usable on any unix system
       # with overlays and any extraModules applied
-      mkHomeConfig = { username, system ? "x86_64-linux"
-        , nixpkgs ? inputs.nixpkgs, stable ? inputs.nixos-stable
-        , lib ? (mkLib nixpkgs), baseModules ? [
+      mkHomeConfig =
+        { username
+        , system ? "x86_64-linux"
+        , nixpkgs ? inputs.nixpkgs
+        , stable ? inputs.stable
+        , baseModules ? [
           ./modules/home-manager
           {
-            home.sessionVariables = {
-              NIX_PATH =
-                "nixpkgs=${nixpkgs}:stable=${stable}\${NIX_PATH:+:}$NIX_PATH";
-             };
+            home = {
+              inherit username;
+              homeDirectory = "${homePrefix system}/${username}";
+              sessionVariables = {
+                NIX_PATH =
+                  "nixpkgs=${nixpkgs}:stable=${stable}\${NIX_PATH:+:}$NIX_PATH";
+              };
+            };
           }
-        ] , extraModules ? [ ] }:
-        homeManagerConfiguration rec {
-          inherit system username;
-          homeDirectory = "${homePrefix system}/${username}";
-          extraSpecialArgs = { inherit inputs lib nixpkgs stable; };
-          configuration = {
-            imports = baseModules ++ extraModules ++ [
-              (import ./modules/overlays.nix { inherit inputs nixpkgs stable; })
-            ];
+        ]
+        , extraModules ? [ ]
+        }:
+        inputs.home-manager.lib.homeManagerConfiguration rec {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = builtins.attrValues self.overlays;
           };
+
+          extraSpecialArgs = { inherit self inputs nixpkgs; };
+          modules = baseModules ++ extraModules;
         };
-    in {
-      checks = listToAttrs (
+    in
+    {
+      checks = builtins.listToAttrs (
         # darwin checks
-        (map (system: {
+        (builtins.map
+          (system: {
           name = system;
           value = {
             darwin =
@@ -117,23 +136,27 @@
             darwinServer =
               self.homeConfigurations.darwinServer.activationPackage;
           };
-        }) lib.platforms.darwin) ++
+          })
+          inputs.nixpkgs.lib.platforms.darwin) ++
         # linux checks
-        (map (system: {
+        (builtins.map
+          (system: {
           name = system;
           value = {
             nixos = self.nixosConfigurations.phil.config.system.build.toplevel;
             server = self.homeConfigurations.server.activationPackage;
           };
-        }) lib.platforms.linux));
+          })
+          inputs.nixpkgs.lib.platforms.linux)
+      );
 
       darwinConfigurations = {
         MacBook = mkDarwinConfig {
           system = "x86_64-darwin";
           extraModules = [
             ./profiles/personal.nix
-            ./modules/darwin/apps.nix
-            { homebrew.brewPrefix = "/usr/local/Homebrew/bin"; }
+            # ./modules/darwin/apps.nix
+            # { homebrew.brewPrefix = "/usr/local/Homebrew/bin"; }
           ];
         };
       };
@@ -142,7 +165,7 @@
         phil = mkNixosConfig {
           hardwareModules = [
             ./modules/hardware/phil.nix
-            nixos-hardware.nixosModules.lenovo-thinkpad-t460s
+            inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t460s
           ];
           extraModules = [ ./profiles/personal.nix ];
         };
@@ -163,37 +186,78 @@
           extraModules = [ ./profiles/home-manager/personal.nix ];
         };
       };
-    } //
-    # add a devShell to this flake
-    eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            devshell.overlay
-            (final: prev: {
-              # expose stable packages via pkgs.stable
-              stable = import inputs.nixos-stable { system = prev.system; };
-            })
-          ];
+
+      devShells = eachSystemMap defaultSystems (system:
+        let
+          pkgs = import inputs.stable {
+            inherit system;
+            overlays = builtins.attrValues self.overlays;
+          };
+        in
+        {
+          default = pkgs.devshell.mkShell {
+            packages = with pkgs; [
+              nixfmt
+              pre-commit
+              self.packages.${system}.pyEnv
+              treefmt
+            ];
+            commands = [{
+              name = "sysdo";
+              package = self.packages.${system}.sysdo;
+              category = "utilities";
+              help = "perform actions on this repository";
+            }];
+          };
+        });
+
+      packages = eachSystemMap defaultSystems (system:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = builtins.attrValues self.overlays;
+          };
+        in
+        rec {
+          pyEnv = pkgs.python3.withPackages
+            (ps: with ps; [ black typer colorama shellingham]);
+          sysdo = pkgs.writeScriptBin "sysdo" ''
+            #! ${pyEnv}/bin/python3
+            ${builtins.readFile ./bin/do.py}
+          '';
+        });
+
+      # apps = eachSystemMap defaultSystems (system: rec {
+      #   sysdo = {
+      #     type = "app";
+      #     program = "${self.packages.${system}.sysdo}/bin/sysdo";
+      #   };
+      #   default = sysdo;
+      # });
+
+      overlays = {
+        channels = final: prev: {
+          # expose other channels via overlays
+          stable = import inputs.stable { system = prev.system; };
+          small = import inputs.small { system = prev.system; };
         };
-        pyEnv = (pkgs.stable.python3.withPackages
-          (ps: with ps; [ black pylint typer colorama shellingham ]));
-        nixBin = pkgs.writeShellScriptBin "nix" ''
-          ${pkgs.nixStable}/bin/nix --option experimental-features "nix-command flakes" "$@"
-        '';
-        sysdo = pkgs.writeShellScriptBin "sysdo" ''
-          cd $PRJ_ROOT && ${pyEnv}/bin/python3 bin/do.py $@
-        '';
-      in {
-        devShell = pkgs.devshell.mkShell {
-          packages = [ nixBin pyEnv pkgs.treefmt pkgs.nixfmt pkgs.stylua ];
-          commands = [{
-            name = "sysdo";
-            package = sysdo;
-            category = "utilities";
-            help = "perform actions on this repository";
-          }];
+        python =
+          let
+            overrides = (pfinal: pprev: {
+              pyopenssl = pprev.pyopenssl.overrideAttrs
+                (old: { meta = old.meta // { broken = false; }; });
+            });
+          in
+          final: prev: {
+            python3 = prev.python3.override { packageOverrides = overrides; };
+            python39 = prev.python39.override { packageOverrides = overrides; };
+            python310 = prev.python310.override { packageOverrides = overrides; };
+          };
+        extraPackages = final: prev: {
+          sysdo = self.packages.${prev.system}.sysdo;
+          pyEnv = self.packages.${prev.system}.pyEnv;
         };
-      });
+        devshell = inputs.devshell.overlay;
+      };
+    };
 }
