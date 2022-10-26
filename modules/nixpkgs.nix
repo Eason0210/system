cfd801259dadf12a383f3b940458d83e9ec092f0
Eason0210 <aqua0210@163.com>
2022-10-26 23:29:05 +0800

    Update modules/nixpkgs.nix

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,24 +1,33 @@
-{ inputs, config, lib, pkgs, nixpkgs, stable, ... }: {
-  nixpkgs = {
-    config = import ./config.nix;
-    overlays = [ ];
-  };
+{ inputs, config, lib, pkgs, ... }: {
+  nixpkgs = { config = import ./config.nix; };
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nix;
     extraOptions = ''
       keep-outputs = true
-      keep-derivations = true
-      ${lib.optionalString (config.nix.package == pkgs.nixFlakes)
-      "experimental-features = nix-command flakes"}
+      keep-derivations =
+      experimental-features = nix-command flakes
     '';
-    trustedUsers = [ "${config.user.name}" "root" "@admin" "@wheel" ];
+
+    settings = {
+      max-jobs = 8;
+      trusted-users = [ "${config.user.name}" "root" "@admin" "@wheel" ];
+      trusted-substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://eason0210.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "eason0210.cachix.org-1:+3t6gpdGWBJcwVoV7gNMSzKCxxlxqS+YrY6YCynvgSg="
+      ];
+    };
     gc = {
       automatic = true;
-      options = "--delete-older-than 30d";
+      options = "--delete-older-than 14d";
     };
-    buildCores = 8;
-    maxJobs = 8;
+
     readOnlyStore = true;
     nixPath = builtins.map
       (source: "${source}=/etc/${config.environment.etc.${source}.target}") [
@@ -26,29 +35,20 @@
       "nixpkgs"
       "stable"
     ];
-
-    binaryCaches =
-      [ "https://eason0210.cachix.org" "https://nix-community.cachix.org/" ];
-    binaryCachePublicKeys = [
-      "eason0210.cachix.org-1:+3t6gpdGWBJcwVoV7gNMSzKCxxlxqS+YrY6YCynvgSg="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-
     registry = {
       nixpkgs = {
         from = {
           id = "nixpkgs";
           type = "indirect";
         };
-        flake = nixpkgs;
+        flake = inputs.nixpkgs;
       };
-
       stable = {
         from = {
           id = "stable";
           type = "indirect";
         };
-        flake = stable;
+        flake = inputs.stable;
       };
     };
   };
